9cc3e567b3a926ba1fdfa49748adf9616ff6cf8f
Garcia Pinto <150735867+Garcia-32@users.noreply.github.com>
2025-09-09 23:30:34 -0500

    Add files via upload

---
+++ archivo/Jugador.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tresenrayaspoo;
+// Clase que representa a un jugador
+public class Jugador {
+ 
+
+    private String nombre;
+    private char ficha;
+
+    public Jugador(String nombre, char ficha) {
+        System.out.println("Creando la clase Jugador para " + nombre + " con ficha " + ficha);
+        this.nombre = nombre;
+        this.ficha = ficha;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public char getFicha() {
+        return ficha;
+    }
+}
+
+ 
